a95ffcb133ab090baf90dee47709aac277a45d86
Kristoffer Kiil <krki@ssi.dk>
2022-11-28 11:47:36 +0100

    Bump version: v1_0_0 → v1_0_1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bifrost_chewbbaca',
-    version='v1_0_0',
+    version='v1_0_1',
     url='https://github.com/ssi-dk/bifrost_chewbbaca',
 
     # Author details

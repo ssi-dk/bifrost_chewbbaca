8fa6cb2a57af44c2c9dc64ddc0f55e5bac5c142f
Karen Loaiza <kloc@ssi.dk>
2023-08-23 15:18:49 +0200

    Replaced dashes _ for dots . in versions for setuptools compatibility

--- setup.py
+++ setup.py
@@ -1,24 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
-    name='bifrost_chewbbaca',
-    version='v1.0.6',
-    url='https://github.com/ssi-dk/bifrost_chewbbaca',
-
-    # Author details
-    author='Kim Ng',
-    author_email='kimn@ssi.dk',
-
-    # Choose your license
-    license='MIT',
-
-    packages=find_packages(),
-    python_requires='>=3.6',
-
-    package_data={'bifrost_chewbbaca': ['config.yaml', 'pipeline.smk']},
+    name="bifrost_chewbbaca",
+    version="1.0.6",
+    url="https://github.com/ssi-dk/bifrost_chewbbaca",
+    author="Kim Ng",
+    author_email="kimn@ssi.dk",
+    license="MIT",
+    packages=find_namespace_packages(),
+    python_requires=">=3.6",
+    package_data={"bifrost_chewbbaca": ["config.yaml", "pipeline.smk"]},
     include_package_data=True,
-
-    install_requires=[
-        'bifrostlib >= 2.0.11'
-    ]
+    install_requires=["bifrostlib >= 2.0.11"],
 )
